dddcb22bf9642626facd8feff4fe14681ec6130f
JoaoIto <joaovictorpfr@gmail.com>
2024-03-05 14:34:13 -0300

    colocando rotas autenticadas

--- src/routes/clientes.ts
+++ src/routes/clientes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { ClientesController } from '../controllers/clientes';
+import {authenticateToken} from "../middleware/authenticateToken";
 
 export const ClientesRouter = express.Router();
 
-ClientesRouter.get('/', ClientesController.listarClientes);
-ClientesRouter.post('/cadastrar', ClientesController.criarClientes);
-ClientesRouter.get('/filtro', ClientesController.filtrarClientes);
+ClientesRouter.get('/', authenticateToken, ClientesController.listarClientes);
+ClientesRouter.post('/cadastrar', authenticateToken, ClientesController.criarClientes);
+ClientesRouter.get('/filtro', authenticateToken, ClientesController.filtrarClientes);
