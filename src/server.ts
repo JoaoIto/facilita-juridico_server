2395da06036de07229185da045e0eb3e5c40835a
JoaoIto <joaovictorpfr@gmail.com>
2024-03-05 13:37:53 -0300

    colocando json parser

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import {ClientesController} from "./controllers/ClientesController";
+import { ClientesRouter } from './routes/clientes';
 
 const app = express();
 const port = 8080;
+
+// Middleware para fazer o parse do corpo da requisição como JSON
+app.use(express.json());
+
 // Habilitar o CORS para todos os endpoints
 app.use(cors());
+
+// Incluir as rotas de clientes
+app.use('/clientes', ClientesRouter);
+
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}\n`);
 });
-
-app.get('/', async (req, res) => {
-    res.json({message: "Hello World!"})
-})
-
-app.get('/clientes', ClientesController.listarClientes);
-
