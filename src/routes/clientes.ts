f80a8daf49ccf751ba51d9837d70f6aab37820b6
JoaoIto <joaovictorpfr@gmail.com>
2024-03-06 10:00:12 -0300

    rota de clientes rota

--- src/routes/clientes.ts
+++ src/routes/clientes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { ClientesController } from '../controllers/clientes';
 import {authenticateToken} from "../middleware/authenticateToken";
+import {RotaController} from "../controllers/rotas";
 
 export const ClientesRouter = express.Router();
 
 ClientesRouter.get('/', authenticateToken, ClientesController.listarClientes);
 ClientesRouter.post('/cadastrar', authenticateToken, ClientesController.criarClientes);
 ClientesRouter.get('/filtro', authenticateToken, ClientesController.filtrarClientes);
+
+// Rota para calcular a rota do caixeiro viajante
+ClientesRouter.get('/rota', authenticateToken, RotaController.calcularRota);
